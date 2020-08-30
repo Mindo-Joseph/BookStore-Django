1d6fed834e1f0c759060c7c8d8d4c34c7f6a4f14
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-08-30 03:10:34 +0300

    chore: Rewrite docker settings

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -80,9 +80,11 @@ WSGI_APPLICATION = 'bookstore_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'funzadb',
-        'USER': 'joe',
-        'PORT': 5432
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
 
 
     }
